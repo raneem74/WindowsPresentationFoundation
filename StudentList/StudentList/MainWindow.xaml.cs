0f5044284f731c0bc0d9ff69385b6b1d1650ff68
raneem74 <119134294+raneem74@users.noreply.github.com>
2023-02-11 19:20:16 +0200

    Add student list

---
+++ StudentList/StudentList/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using StudentList;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace StudentList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<student> stList = new List<student>();
+            stList.Add(new student { id = 1, name = "John Doe", age = 22, grade = 89, image = "D:\\1-ITI\\ITI_WPF\\lab3\\StudentList\\StudentList\\images\\1.png" });
+            stList.Add(new student { id = 2, name = "Jane Doe", age = 21, grade = 92, image = "D:\\1-ITI\\ITI_WPF\\lab3\\StudentList\\StudentList\\images\\2.png" });
+            stList.Add(new student { id = 3, name = "Bob Smith", age = 23, grade = 75, image = "D:\\1-ITI\\ITI_WPF\\lab3\\StudentList\\StudentList\\images\\3.png" });
+            stList.Add(new student { id = 4, name = "Eimagesa Watson", age = 24, grade = 95, image = "D:\\1-ITI\\ITI_WPF\\lab3\\StudentList\\StudentList\\images\\4.png" });
+            stList.Add(new student { id = 5, name = "Michael Scott", age = 25, grade = 76, image = "D:\\1-ITI\\ITI_WPF\\lab3\\StudentList\\StudentList\\images\\1.png" });
+            stList.Add(new student { id = 6, name = "Sarah Johnson", age = 26, grade = 87, image = "D:\\1-ITI\\ITI_WPF\\lab3\\StudentList\\StudentList\\images\\2.png" });
+            stList.Add(new student { id = 7, name = "David Smith", age = 27, grade = 91, image = "D:\\1-ITI\\ITI_WPF\\lab3\\StudentList\\StudentList\\images\\3.png" });
+            stList.Add(new student { id = 8, name = "Emily Davis", age = 28, grade = 80, image = "D:\\1-ITI\\ITI_WPF\\lab3\\StudentList\\StudentList\\images\\4.png" });
+            namesList.ItemsSource = stList;
+
+        }
+
+
+
+    }
+}
---
+++ StudentList/StudentList/student.cs
@@ -0,0 +1,28 @@
+﻿using StudentList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace StudentList
+{
+    internal class student
+    {
+ 
+        
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public int grade { get; set; }
+        public string image { get; set; }
+
+        public override string ToString()
+        {
+            return name.ToString();
+        }
+    }
+
+    
+}
